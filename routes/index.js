14d1af51068a9fb44a3d2845e12670ff38a2dd7f
Santos <santalinos@outlook.es>
2015-06-04 14:34:54 +0200

    Creación dela primera pregunta

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+var quizController = require('../controllers/quiz_controller');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+/* Quizes.Question. */
+router.get('/quizes/question', quizController.question);
+
+/* Quizes.Answer. */
+router.get('/quizes/answer', quizController.answer);
 
 module.exports = router;
